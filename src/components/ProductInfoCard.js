6ac17af9edfbee2d9a689df013fbdd062c81b2c5
NayMyoMin <naymyomin.nayy@gmail.com>
2020-11-01 00:34:40 +0630

    added api get connection

--- src/components/ProductInfoCard.js
+++ src/components/ProductInfoCard.js
@@ -1,57 +1,57 @@
-import { React } from 'react';
-import './Components.css';
-import 'antd/dist/antd.css';
-import { Card, Descriptions, Button } from 'antd';
-import { EditOutlined } from '@ant-design/icons';
+import { React } from 'react'
+import './Components.css'
+import 'antd/dist/antd.css'
+import { Card, Descriptions, Button } from 'antd'
+import { EditOutlined } from '@ant-design/icons'
 
 const ProductInfoCard = ({ product }) => {
-	return (
-		<div style={{ paddingTop: '86px' }}>
-			<Card
-				title={product.name}
-				style={{
-					width: '400px',
-					margin: '24px',
-				}}
-			>
-				<img
-					height="150px"
-					width="150px"
-					style={{
-						marginBottom: '24px',
-						objectFit: 'cover',
-					}}
-					alt="Product Item"
-					src={product.imgUrl}
-				/>
-				<Card.Meta
-					description={
-						<>
-							<Descriptions column={1} size="small">
-								<Descriptions.Item label="Product Name">
-									{product.name}
-								</Descriptions.Item>
-								<Descriptions.Item label="Price">
-									{product.price}
-								</Descriptions.Item>
-								<Descriptions.Item label="Stock Balance">
-									{product.stockBalance}
-								</Descriptions.Item>
-								<Descriptions.Item label="Warehouse">
-									{product.warehouse}
-								</Descriptions.Item>
-								<Descriptions.Item label="Description">
-									{product.description}
-								</Descriptions.Item>
-							</Descriptions>
-							<br />
-							<Button icon={<EditOutlined />}>Edit</Button>
-						</>
-					}
-				/>
-			</Card>
-		</div>
-	);
-};
+  return (
+    <div style={{ paddingTop: '86px' }}>
+      <Card
+        title={product.name}
+        style={{
+          width: '400px',
+          margin: '24px'
+        }}
+      >
+        <img
+          height="150px"
+          width="150px"
+          style={{
+            marginBottom: '24px',
+            objectFit: 'cover'
+          }}
+          alt="Product Item"
+          src={product.picture}
+        />
+        <Card.Meta
+          description={
+            <>
+              <Descriptions column={1} size="small">
+                <Descriptions.Item label="Product Name">
+                  {product.name}
+                </Descriptions.Item>
+                <Descriptions.Item label="Price">
+                  {product.price}
+                </Descriptions.Item>
+                <Descriptions.Item label="Stock Balance">
+                  {product.stockBalance}
+                </Descriptions.Item>
+                <Descriptions.Item label="Warehouse">
+                  {product.warehouse}
+                </Descriptions.Item>
+                <Descriptions.Item label="Description">
+                  {product.description}
+                </Descriptions.Item>
+              </Descriptions>
+              <br />
+              <Button icon={<EditOutlined />}>Edit</Button>
+            </>
+          }
+        />
+      </Card>
+    </div>
+  )
+}
 
-export default ProductInfoCard;
+export default ProductInfoCard
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,4 +1,5 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
+import axios from 'axios'
 import 'antd/dist/antd.css'
 import { Table, Typography, Row } from 'antd'
 import './Components.css'
@@ -7,9 +8,9 @@ import ProductActions from './ProductActions'
 
 const columns = [
   {
-    title: 'No.',
-    dataIndex: 'id',
-    key: 1
+    title: 'No',
+    key: 'index',
+    render: (text, record, index) => index + 1
   },
   {
     title: 'Product Name',
@@ -30,7 +31,7 @@ const columns = [
   },
   {
     title: 'Warehouse',
-    dataIndex: 'warehouse',
+    dataIndex: 'warehouseId',
     key: 'warehouse'
   }
 ]
@@ -41,9 +42,9 @@ const items = [
     name: 'Product 1',
     price: 10000,
     stockBalance: 250,
-    warehouse: 'Warehouse 1',
+    warehouse: 'Warehouse 4',
     description: 'this item is blah blah',
-    imgUrl:
+    picture:
       'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
   },
   {
@@ -53,16 +54,16 @@ const items = [
     stockBalance: 100,
     warehouse: 'Warehouse 3',
     description: 'this item is blah blah',
-    imgUrl: 'https://homepages.cae.wisc.edu/~ece533/images/watch.png'
+    picture: 'https://homepages.cae.wisc.edu/~ece533/images/watch.png'
   },
   {
     id: 3,
     name: 'Product 5',
     price: 50000,
     stockBalance: 100,
-    warehouse: 'Yangon Warehouse',
+    warehouse: 'Warehouse 2',
     description: 'this item is blah blah',
-    imgUrl:
+    picture:
       'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
   },
   {
@@ -72,12 +73,25 @@ const items = [
     stockBalance: 80,
     warehouse: 'Warehouse 1',
     description: 'Product of Myanmar',
-    imgUrl: 'https://homepages.cae.wisc.edu/~ece533/images/fruits.png'
+    picture: 'https://homepages.cae.wisc.edu/~ece533/images/fruits.png'
   }
 ]
 
 const ProductTable = () => {
   const [product, setProduct] = useState(items[0])
+
+  const [apiProducts, setApiProducts] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('/api/v1/products')
+      .then(response => {
+        setApiProducts(response.data)
+      })
+      .catch(() => {
+        console.log('failed to fetch products')
+      })
+  }, []) // <= just copied your code
 
   const onRowClick = record => ({
     onClick: () => {
@@ -93,7 +107,7 @@ const ProductTable = () => {
         <ProductActions />
         <Table
           columns={columns}
-          dataSource={items}
+          dataSource={apiProducts}
           rowKey={item => item.id}
           scroll={{ x: 240 }}
           onRow={onRowClick}
